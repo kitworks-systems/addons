bab6f4290004a84088da41b642f5b29f51d6f4cb
Tymur Ivasiuk <ivasiuktymur@gmail.com>
2024-04-12 06:28:40 +0000

    [ADD] kw_tree_checkbox_sticky,kw_tree_header_sticky

---
+++ kw_tree_header_sticky/__manifest__.py
@@ -0,0 +1,15 @@
+
+{
+    'name': 'Tree Header Sticky',
+    'version': '16.0.0.1.0',
+    'category': 'Other',
+    'depends': [
+
+    ],
+    'license': 'OPL-1',
+    'assets': {
+        'web.assets_backend': [
+            'kw_tree_header_sticky/static/src/css/header_sticky.css'
+        ]
+    }
+}
