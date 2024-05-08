2bcf2fe31408335acc6075122a91ea689527070a
Володимир Карабанов <karabanov.vladimir@gmail.com>
2024-05-08 13:42:36 +0000

    15.0 oh forward port from 14.0 x n jb7

---
+++ __obsolete__/kw_widget_many2one_reference/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Widget to field many2one_reference",
+    'version': '15.0.1.1.0',
+
+    'author': 'Kitworks Systems',
+    'website': 'https://kitworks.systems/',
+    'license': 'OPL-1',
+    'category': 'Extra Tools',
+
+    'depends': ['web', ],
+
+    'data': [],
+}
