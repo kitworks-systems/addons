15c3007592d0cdcd745dccf3673423ec43f61bd7
Oleksandr Nasalskyi <nasalskyi@maao.int>
2024-10-01 07:48:54 +0000

    Revert "Merge branch '16.0-t1' into '14.0'"
    This reverts merge request !24

--- __obsolete__/kw_widget_many2one_reference/__manifest__.py
+++ __obsolete__/kw_widget_many2one_reference/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Widget to field many2one_reference",
-    'version': '16.0.2.0.1',
+    'version': '14.0.2.0.0',
 
     'author': 'Kitworks Systems',
     'website': 'https://kitworks.systems/',
